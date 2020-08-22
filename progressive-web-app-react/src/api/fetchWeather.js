6ef294c487bf1746d0c91ecb27a4eebb63fa84c9
Mark Leo <ratrateroo@gmail.com>
2020-08-22 23:11:11 +0800

    added  api

--- progressive-web-app-react/src/api/fetchWeather.js
+++ progressive-web-app-react/src/api/fetchWeather.js
@@ -1,1 +1,10 @@
 import axios from 'axios';
+
+const URL = 'https://api.openweathermap.org/data/2.5/weather/';
+//const  API_KEY =  
+
+const fetchWeather =  async (query) => {
+    const response = await axiois.get(URL);
+}
+
+export default fetchWeather;
