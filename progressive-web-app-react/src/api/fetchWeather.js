370c60a8dae1e3809eae1d7200c3ed9640740e05
Mark Leo <ratrateroo@gmail.com>
2020-08-24 00:00:16 +0800

    added needed axios package

--- progressive-web-app-react/src/api/fetchWeather.js
+++ progressive-web-app-react/src/api/fetchWeather.js
@@ -3,7 +3,9 @@ import axios from 'axios';
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
 const  API_KEY = 'f33a484cf794d08d0148764789aaba32';
 
-const fetchWeather =  async (query) => {
+
+
+export const fetchWeather =  async (query) => {
     const { data } = await axios.get(URL, {
         q: query,
         units: 'metric',
@@ -11,4 +13,3 @@ const fetchWeather =  async (query) => {
     });
 }
 
-export default fetchWeather;
