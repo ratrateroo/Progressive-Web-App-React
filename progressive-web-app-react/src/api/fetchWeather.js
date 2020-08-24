bdd6ebba3821e17e3c44eb3078fb6418de458046
Mark Leo <ratrateroo@gmail.com>
2020-08-24 10:49:42 +0800

    fixed some connection problem

--- progressive-web-app-react/src/api/fetchWeather.js
+++ progressive-web-app-react/src/api/fetchWeather.js
@@ -1,15 +1,20 @@
 import axios from 'axios';
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
-const  API_KEY = 'f33a484cf794d08d0148764789aaba32';
+const  API_KEY = '38b0adfe83945b38e59aa0d4ca87362a';
 
 
 
 export const fetchWeather =  async (query) => {
     const { data } = await axios.get(URL, {
-        q: query,
-        units: 'metric',
-        APPID: API_KEY,
+        params: {
+
+            q: query,
+            units: 'metric',
+            APPID: API_KEY,
+        }
     });
+
+    return data;
 }
 
